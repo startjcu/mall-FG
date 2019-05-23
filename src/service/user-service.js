385a03c110134f22bf14a62558cdcd0842a46958
star <startjcu@163.com>
2019-05-23 16:56:00 +0800

    通用导航逻辑层开发

---
+++ src/service/user-service.js
@@ -0,0 +1,24 @@
+var _mm = require('util/_mm.js')
+
+var _user = {
+  //退出登录
+  logout: function (resolve, reject) {
+    _mm.request({
+      url: _mm.getServerUrl('/user/logout.do'),
+      method: 'POST',
+      success: resolve,
+      error: reject
+    })
+  },
+  //检查登录状态
+  checkLogin: function (resolve, reject) {
+    _mm.request({
+      url: _mm.getServerUrl('/user/get_user_info.do'),
+      method: 'POST',
+      success: resolve,
+      error: reject
+    })
+  }
+}
+
+module.exports = _user
